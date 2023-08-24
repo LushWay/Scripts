765a0f009df7d71145a63fbb56ed46fb6551d766
shp1nat00 <bomj.valera.lerok@gmail.com>
2023-08-24 15:10:02 +0300

    Co-authored-by: Leaftail <leaftail1880@users.noreply.github.com>

--- scripts/modules/Server/Menu/index.js
+++ scripts/modules/Server/Menu/index.js
@@ -3,26 +3,26 @@ import { XEntity, util } from "xapi.js";
 import { MENU } from "./var.js";
 
 world.afterEvents.itemUse.subscribe(async ({ source: player, itemStack }) => {
-  if (itemStack.typeId !== MENU.item.typeId || !(player instanceof Player))
-    return;
+	if (itemStack.typeId !== MENU.item.typeId || !(player instanceof Player))
+		return;
 
-  util.catch(() => {
-    const menu = MENU.OnOpen(player);
-    if (menu === false) return;
-    menu.show(player);
-  });
+	util.catch(() => {
+		const menu = MENU.OnOpen(player);
+		if (menu === false) return;
+		menu.show(player);
+	});
 });
 
 new XCommand({
-  name: "menu",
-  description: "Выдает/убирает меню из инвентаря",
-  type: "public",
+	name: "menu",
+	description: "Выдает/убирает меню из инвентаря",
+	type: "public",
 }).executes(async (ctx) => {
-  if (await XEntity.hasItem(ctx.sender, 0, MENU.item.typeId)) {
-    ctx.sender.runCommandAsync(`clear @s ${MENU.item}`);
-    ctx.reply("§c- §3Меню");
-  } else {
-    ctx.sender.getComponent("inventory").container.addItem(MENU.item);
-    ctx.reply("§a+ §3Меню");
-  }
+	if (await XEntity.hasItem(ctx.sender, 0, MENU.item.typeId)) {
+		ctx.sender.runCommandAsync(`clear @s ${MENU.item}`);
+		ctx.reply("§c- §3Меню");
+	} else {
+		ctx.sender.getComponent("inventory").container.addItem(MENU.item);
+		ctx.reply("§a+ §3Меню");
+	}
 });
