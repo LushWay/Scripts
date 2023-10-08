633617956642803517b7919c3ceb6f664e4d0af3
leaftail1880 <110915645+leaftaul1880@users.noreply.github.com>
2023-10-08 22:35:15 +0300

    fix

--- scripts/modules/Development/WorldEdit/commands/tools/nylium.js
+++ scripts/modules/Development/WorldEdit/commands/tools/nylium.js
@@ -36,22 +36,20 @@ const nylium = new WorldEditTool({
 world.beforeEvents.playerPlaceBlock.subscribe(({ block, player }) => {
 	if (block.typeId !== nylium.item) return;
 
-	system.run(() =>
-		util.catch(() => {
-			const slot = player
-				.getComponent("equippable")
-				.getEquipmentSlot(EquipmentSlot.Mainhand);
-			const blocksSets = getBlockSets(player);
-			const name = nylium.parseLore(slot.getLore())?.blocksSet;
+	system.run(() => {
+		const slot = player
+			.getComponent("equippable")
+			.getEquipmentSlot(EquipmentSlot.Mainhand);
+		const blocksSets = getBlockSets(player);
+		const name = nylium.parseLore(slot.getLore())?.blocksSet;
 
-			if (name in blocksSets) {
-				setblock(getBlockSet(blocksSets, name).randomElement(), block.location);
-			} else {
-				player.tell(
-					"§cНеизвестный набор блоков! Выберите существующий из списка",
-				);
-				nylium.editToolForm(slot, player);
-			}
-		}),
-	);
+		if (name in blocksSets) {
+			setblock(getBlockSet(blocksSets, name).randomElement(), block.location);
+		} else {
+			player.tell(
+				"§cНеизвестный набор блоков! Выберите существующий из списка",
+			);
+			nylium.editToolForm(slot, player);
+		}
+	});
 });
