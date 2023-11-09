fd34fc8c08ca470addc3a2481243598806637039
leaftail1880 <110915645+leaftaul1880@users.noreply.github.com>
2023-11-09 19:03:27 +0300

    use without inv

--- tools/chestui.js
+++ tools/chestui.js
@@ -4,8 +4,8 @@ import { defineGitDependency } from 'leafy-utils'
 export default defineGitDependency({
   remote: {
     url: 'https://github.com/Herobrine643928/Chest-UI',
-    branch: 'main',
-    path: 'BP/scripts/',
+    branch: 'additional-features',
+    path: 'Without Inventory/BP/scripts/',
   },
   path: 'scripts/',
   dependencies: {
