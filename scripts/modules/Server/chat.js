9a8feca866503527282af9b2cf04a0e429b40670
leaftail1880 <110915645+leaftaul1880@users.noreply.github.com>
2024-02-12 19:22:32 +0300

    0.17.34

--- scripts/modules/Server/chat.js
+++ scripts/modules/Server/chat.js
@@ -32,22 +32,22 @@ export class ChatBuilder {
   })
 
   constructor() {
-    world.afterEvents.chatSend.subscribe(data => {
-      if (data.message.startsWith(CONFIG.commandPrefix) && data.message !== CONFIG.commandPrefix) return
+    world.afterEvents.chatSend.subscribe(event => {
+      if (event.message.startsWith(CONFIG.commandPrefix) && event.message !== CONFIG.commandPrefix) return
 
       try {
         const cooldown = this.settings.cooldown
 
         // Is cooldown enabled?
         if (cooldown) {
-          const cool = new Cooldown(this.db, 'CD', data.sender, cooldown)
+          const cool = new Cooldown(this.db, 'CD', event.sender, cooldown)
 
           // Player is under chat cooldown, show error message
           if (cool.isExpired()) return
           cool.update()
         }
 
-        const playerRole = getRole(data.sender)
+        const playerRole = getRole(event.sender)
 
         let role = ''
         if (this.settings.ranks && playerRole !== 'member') {
@@ -57,22 +57,27 @@ export class ChatBuilder {
         const allPlayers = world.getAllPlayers()
 
         // Players that are near message sender
-        const nearPlayers = data.sender.dimension
+        const nearPlayers = event.sender.dimension
           .getPlayers({
-            location: data.sender.location,
+            location: event.sender.location,
             maxDistance: this.settings.range,
           })
-          .filter(e => e.id !== data.sender.id)
+          .filter(e => e.id !== event.sender.id)
 
         // Array with ranged players (include sender id)
         const nID = nearPlayers.map(e => e.id)
-        nID.push(data.sender.id)
+        nID.push(event.sender.id)
 
         // Outranged players
         const otherPlayers = allPlayers.filter(e => !nID.includes(e.id))
-        const messageText = data.message.replace(/\\n/g, '\n')
-        const message = `${role}§7${data.sender.name}§r: ${messageText}`
-        if (util.settings.BDSMode) console.info(message)
+        const messageText = event.message.replace(/\\n/g, '\n')
+        const message = `${role}§7${event.sender.name}§r: ${messageText}`
+
+        if (util.settings.BDSMode) {
+          // This is handled/parsed by ServerCore
+          // Dont really want to do APICall each time here
+          console.info('[Chat] ' + JSON.stringify({ from: event.sender.name, role, messageText, message }))
+        }
 
         for (const near of nearPlayers) {
           near.tell(message)
@@ -80,10 +85,10 @@ export class ChatBuilder {
           if (!this.playerSettings(near).disableSound) near.playSound(SOUNDS.click)
         }
 
-        for (const outranged of otherPlayers) outranged.tell(`${role}§8${data.sender.name}§7: ${messageText}`)
+        for (const outranged of otherPlayers) outranged.tell(`${role}§8${event.sender.name}§7: ${messageText}`)
 
-        const hightlight = this.playerSettings(data.sender).hightlightMessages
-        data.sender.tell(hightlight ? `§6§lЯ§r: §f${messageText}` : message)
+        const hightlight = this.playerSettings(event.sender).hightlightMessages
+        event.sender.tell(hightlight ? `§6§lЯ§r: §f${messageText}` : message)
       } catch (error) {
         util.error(error)
       }
