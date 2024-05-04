5e7d2922cb2c8e0118251198dd182b3bdcb7f8b7
Milk-Cool <milan-pl@mail.ru>
2024-05-04 19:50:25 +0300

    rewards update

--- scripts/lib/Rewards.js
+++ scripts/lib/Rewards.js
@@ -1,8 +1,18 @@
 import { ItemStack, Player } from '@minecraft/server'
+import { emoji } from 'lib/Assets/emoji.js'
+import { itemLocaleName } from './GameUtils'
 
-/** @typedef {{ type: ('scores'|'item'), count: number, id?: string, score?: import('@minecraft/server').ScoreName }} Reward */
+/**
+ * @typedef {{
+ *   type: 'scores' | 'item'
+ *   count: number
+ *   id?: string
+ *   name?: string
+ *   score?: import('@minecraft/server').ScoreName
+ * }} Reward
+ */
 export class Rewards {
-  /** 
+  /**
    * @private
    * @type {Reward[]}
    */
@@ -10,7 +20,7 @@ export class Rewards {
 
   /**
    * Adds a reward entry of leaves or money
-   * 
+   *
    * @param {import('@minecraft/server').ScoreName} type The reward type
    * @param {number} count The reward amount
    * @returns {Rewards}
@@ -19,22 +29,23 @@ export class Rewards {
     this.entries.push({ type: 'scores', count, score: type })
     return this
   }
-  
+
   /**
    * Adds a reward entry of items
-   * 
+   *
    * @param {string} id The item ID
+   * @param {string} name The item name
    * @param {number} count The item count
    * @returns {Rewards}
    */
-  item(id, count) {
-    this.entries.push({ type: 'item', id, count })
+  item(id, name, count) {
+    this.entries.push({ type: 'item', id, name, count })
     return this
   }
 
-  /** 
+  /**
    * Gives a reward to the player
-   * 
+   *
    * @private
    * @param {Player} player The player to give out the rewards to
    * @param {Reward} reward
@@ -43,27 +54,27 @@ export class Rewards {
     if (reward.type == 'item' && reward.id) {
       if (!player.container) return
       player.container.addItem(new ItemStack(reward.id, reward.count))
-    } else if(reward.type == 'scores') {
-      if(!reward.score) return
+    } else if (reward.type == 'scores') {
+      if (!reward.score) return
       player.scores[reward.score] += reward.count
     }
   }
 
   /**
    * Gives out the rewards to a player
-   * 
+   *
    * @param {Player} player The player to give out the rewards to
    * @returns {Rewards}
    */
   give(player) {
-    for(let reward of this.entries)
-      Rewards.giveOne(player, reward)
+    for (const reward of this.entries) Rewards.giveOne(player, reward)
+    player.sendMessage('§l§eВы получили награды!')
     return this
   }
 
   /**
    * Serializes the object into an array
-   * 
+   *
    * @returns {Reward[]} The array of rewards
    */
   serialize() {
@@ -72,7 +83,7 @@ export class Rewards {
 
   /**
    * Restores the object from an array
-   * 
+   *
    * @param {Reward[]} rewards The array of rewards
    * @returns {Rewards}
    */
@@ -80,4 +91,23 @@ export class Rewards {
     this.entries = rewards
     return this
   }
+
+  /**
+   * Returns the rewards as a human-readable string
+   *
+   * @returns {string}
+   */
+  toString() {
+    const rewardsAsStrings = []
+    for (const reward of this.entries) {
+      if (reward.type == 'scores' && reward.score == 'leafs')
+        rewardsAsStrings.push(`${emoji.leaf} Листья x ${reward.count}`)
+      else if (reward.type == 'scores' && reward.score == 'money')
+        rewardsAsStrings.push(`${emoji.money} Монеты x ${reward.count}`)
+      else if (reward.type == 'item') rewardsAsStrings.push(`${reward.name ?? 'Яблоко'} x ${reward.count}`)
+      else rewardsAsStrings.push(`${reward.score} x ${reward.count}`)
+    }
+    return rewardsAsStrings.join('\n')
+  }
 }
+
