695b991b1fc5e691bbd97e7670bb28769e9dde34
Leaftail <110915645+leaftail1880@users.noreply.github.com>
2024-05-17 10:31:28 +0300

    Delete src/modules/Commands directory

--- src/modules/Commands/camera.ts
+++
@@ -1,5 +0,0 @@
-import { restorePlayerCamera } from 'lib'
-
-new Command('camera').setDescription('Возвращает камеру в исходное состояние').executes(ctx => {
-  restorePlayerCamera(ctx.player, 1)
-})
