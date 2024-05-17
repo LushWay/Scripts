f8ad9bc1b5643ad1f12d299915334d1d722f1262
Leaftail <110915645+leaftail1880@users.noreply.github.com>
2024-05-17 10:31:43 +0300

    Delete src/modules/Indicator directory

--- src/modules/Indicator/config.ts
+++
@@ -1,6 +0,0 @@
-export class HealthIndicatorConfig {
-  /** Array of player ids who wouldn't get pvp lock */
-  static disabled: string[] = []
-
-  static lockDisplay: Record<string, number> = {}
-}
