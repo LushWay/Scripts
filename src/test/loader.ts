dd403701606f0dc74285f7d4ce92c075d4df900b
leaftail1880 <110915645+leaftaul1880@users.noreply.github.com>
2024-05-17 10:01:57 +0300

    tests: add RadiusRegionWithStructure tests

--- src/test/loader.ts
+++ src/test/loader.ts
@@ -3,6 +3,7 @@ import './framework'
 
 import 'lib/bds/api.spec'
 import 'lib/command/index.spec'
+import 'lib/region/kinds/RadiusRegionWithStructure.spec'
 import 'lib/temporary.spec'
 import 'modules/places/base/base.spec'
 import 'modules/survival/death-quest-and-gravestone.spec'
