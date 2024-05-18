6297c5229d4df828177b9ce0757f290480c3489b
leaftail1880 <110915645+leaftaul1880@users.noreply.github.com>
2024-05-18 13:55:11 +0300

    cleanup

--- src/modules/quests/index.ts
+++ src/modules/quests/index.ts
@@ -5,13 +5,4 @@ import './learning/index'
 import './lib/cutscene'
 import './lib/cutscene-menu'
 
-import { system } from '@minecraft/server'
 import '../places/stone-quarry/quests/investigating'
-
-system.runInterval(
-  () => {
-    throw new TypeError('Test scripts error!')
-  },
-  'aaa',
-  40,
-)
