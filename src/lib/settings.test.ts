dc1a739ae40ca74d5c03581b5c593f93582eab93
leaftail1880 <110915645+leaftaul1880@users.noreply.github.com>
2024-05-22 10:57:33 +0300

    add settigns tests

---
+++ src/lib/settings.test.ts
@@ -0,0 +1,25 @@
+import { describe, expect, it, vi } from 'vitest'
+import { Settings } from './settings'
+
+describe('setting change events', () => {
+  it('should emit events on change', () => {
+    const onChange = vi.fn()
+    const settings = Settings.world('group1', {
+      name1: {
+        name: 'name',
+        description: 'description',
+        value: true,
+        onChange,
+      },
+    })
+
+    settings.name1 = false
+    expect(onChange).toHaveBeenCalledOnce()
+
+    settings.name1 = true
+    expect(onChange).toHaveBeenCalledTimes(2)
+
+    settings.name1 = true
+    expect(onChange).toHaveBeenCalledTimes(3)
+  })
+})
