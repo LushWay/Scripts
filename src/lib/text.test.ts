e93bf7c03365fb765eac04709865a85fb9f29293
leaftail1880 <110915645+leaftaul1880@users.noreply.github.com>
2024-05-30 22:20:57 +0300

    add new text types

--- src/lib/text.test.ts
+++ src/lib/text.test.ts
@@ -8,6 +8,7 @@ import { beforeEach, describe, expect, it } from 'vitest'
 import { setRole } from './roles'
 import './text'
 import { t, textTable } from './text'
+import { util } from './util'
 
 let player: Player
 beforeEach(() => {
@@ -24,7 +25,7 @@ describe('text', () => {
 
   it('should create text with roles', () => {
     setRole(player, 'admin')
-    expect(t.roles`Строка с ${player}`).toMatchInlineSnapshot(`"§7Строка с §5Админ§r §fTest player name§7§7"`)
+    expect(t.roles`Строка с ${player}`).toMatchInlineSnapshot(`"§7Строка с §5Админ§r §fTest player name§7"`)
   })
 
   it('should create nested text', () => {
@@ -33,7 +34,7 @@ describe('text', () => {
 
   it('should apply options', () => {
     expect(t.options({ unitColor: '§g', textColor: '§4' }).roles`Все должно работать ${player}`).toMatchInlineSnapshot(
-      `"§4Все должно работать §5Админ§r §gTest player name§4§4"`,
+      `"§4Все должно работать §5Админ§r §gTest player name§4"`,
     )
   })
 
@@ -41,36 +42,57 @@ describe('text', () => {
     expect(
       t.error`Много текста ${'длинной'} и полезной в ${5} степени ошибки, произведенной игроком ${player}`,
     ).toMatchInlineSnapshot(
-      `"§cМного текста §fдлинной§c и полезной в §65§r§c степени ошибки, произведенной игроком §fTest player name§c§c"`,
+      `"§7Много текста §fдлинной§7 и полезной в §65§r§7 степени ошибки, произведенной игроком §fTest player name§7"`,
     )
   })
 
   it('should stringify command', () => {
-    expect(t`Используй ${new Command('name')}`).toMatchInlineSnapshot(`"§7Используй §f.name§7§7"`)
+    expect(t`Используй ${new Command('name')}`).toMatchInlineSnapshot(`"§7Используй §f.name§7"`)
   })
 
   it('should stringify object', () => {
     expect(t`Объекты то тоже сюда кидают: ${{ value: true }}`).toMatchInlineSnapshot(`
       "§7Объекты то тоже сюда кидают: {
         value: §6true§r
-      }§7§7"
+      }§7"
     `)
   })
 
   it('should stringify symbol', () => {
     expect(t`Символы то тоже сюда кидают: ${Symbol('test')}`).toMatchInlineSnapshot(
-      `"§7Символы то тоже сюда кидают: §c<>§r§7§7"`,
+      `"§7Символы то тоже сюда кидают: §c<>§r§7"`,
     )
   })
 
   it('should stringify function', () => {
     expect(t`Фнукции то тоже сюда кидают: ${() => {}}`).toMatchInlineSnapshot(
-      `"§7Фнукции то тоже сюда кидают: §c<>§r§7§7"`,
+      `"§7Фнукции то тоже сюда кидают: §c<>§r§7"`,
     )
   })
 
   it('should stringify undefined', () => {
-    expect(t`Пустоту то тоже сюда кидают: ${undefined}`).toMatchInlineSnapshot(`"§7Пустоту то тоже сюда кидают: §7§7"`)
+    expect(t`Пустоту то тоже сюда кидают: ${undefined}`).toMatchInlineSnapshot(`"§7Пустоту то тоже сюда кидают: §7"`)
+  })
+
+  it('should stringify in error mode with nested options', () => {
+    expect(t.error)
+  })
+
+  it('should stringify num with plurals', () => {
+    const n = 10
+
+    const text = util.ngettext(n, ['блок', 'блока', 'блоков'])
+    expect(`§7Было сломано §6${n} §7${text}`).toMatchInlineSnapshot(`"§7Было сломано §610 §7блоков"`)
+
+    expect(t.num`Было сломано ${n} ${['блок', 'блока', 'блоков']}`).toMatchInlineSnapshot(
+      `"§7Было сломано §610§7 блоков"`,
+    )
+  })
+
+  it('should work with badge', () => {
+    expect(t.badge`Письма ${-3}`).toMatchInlineSnapshot(`"§7Письма§7"`)
+    expect(t.badge`Письма ${0}`).toMatchInlineSnapshot(`"§7Письма§7"`)
+    expect(t.badge`Письма ${3}`).toMatchInlineSnapshot(`"§7Письма §8(§c3§8)§7"`)
   })
 })
 
--- src/lib/text.ts
+++ src/lib/text.ts
@@ -4,46 +4,80 @@ import { ROLES, getRole } from './roles'
 import { util } from './util'
 
 export type Text = string
+
 interface TStatic {
   error: TFn & Omit<TStatic, 'error'>
   header: TFn
   roles: (text: TemplateStringsArray, ...players: Player[]) => Text
   badge: (text: TemplateStringsArray, n: number) => Text
-  plurals: (text: TemplateStringsArray, n: number, plurals: [one: string, two: string, five: string]) => Text
+  num: (text: TemplateStringsArray, n: number, plurals: Plurals) => Text
   time: (text: TemplateStringsArray, time: number) => Text
   options: (options: ColorizingOptions) => TStatic
 }
 
 type TFn = (text: TemplateStringsArray, ...args: unknown[]) => Text
 
-function createSingleConcat(
-  options?: ColorizingOptions,
-  fn = (string: string, arg: unknown) => string + textUnitColorize(arg, options),
-) {
-  const textColor = options?.textColor ?? '§7'
-  return function t(text, ...args) {
-    return text.raw.reduce((previous, string, i) => previous + fn(string, args[i]) + textColor, textColor)
-  } as TFn & TStatic
-}
-
-function createMultiConcat(options: ColorizingOptions = {}) {
-  const t = createSingleConcat(options)
-  t.roles = createSingleConcat({ roles: true, ...options })
-  t.header = createSingleConcat({ textColor: '§6', unitColor: '§f§l', ...options })
-  t.badge = createSingleConcat({ badge: true, ...options })
-  t.plurals = createSingleConcat({ plurals: true, ...options })
-  t.time = createSingleConcat({ time: true, ...options })
-  t.error = createSingleConcat({ textColor: '§c', unitColor: '§f', ...options })
-  t.options = options => createMultiConcat(options)
-  return t
-}
-
-export const t = createMultiConcat()
-
 export function textTable(table: Record<string, unknown>) {
   return Object.entries(table)
     .map(([key, value]) => `§7${key}: ${textUnitColorize(value)}`)
     .join('\n')
+}
+
+export const t = createMultiConcat()
+
+function createMultiConcat(options: ColorizingOptions = {}, name?: keyof TStatic) {
+  options.textColor ??= '§7'
+  const t = createSingleConcat(options)
+  t.roles = createSingleConcat({ roles: true, ...options })
+  t.header = createSingleConcat({ textColor: '§6', unitColor: '§f§l', ...options })
+  t.badge = createBadgeConcat(options)
+  t.num = createNumConcat(options)
+  t.time = createSingleConcat({ time: true, ...options })
+
+  if (name !== 'error') t.error = createMultiConcat({ textColor: '§c', unitColor: '§f', ...options }, 'error')
+  t.options = options => createMultiConcat(options)
+  return t
+}
+
+function createSingleConcat(
+  options?: ColorizingOptions,
+  fn = (string: string, arg: unknown, i: number, array: readonly unknown[]) => string + textUnitColorize(arg, options),
+) {
+  const textColor = options?.textColor ?? '§7'
+  return function t(text, ...args) {
+    const raw = text.raw.slice()
+    if (raw.at(-1) === '') raw.pop()
+    return raw.reduce((previous, string, i, arr) => previous + fn(string, args[i], i, args) + textColor, textColor)
+  } as TFn & TStatic
+}
+
+function createBadgeConcat(options: ColorizingOptions): (text: TemplateStringsArray, n: number) => Text {
+  return createSingleConcat(options, (string, arg) => {
+    if (typeof arg === 'number') {
+      if (arg > 0) return string + '§8(' + '§c' + arg + '§8)'
+      return string.trimEnd()
+    }
+    return string + textUnitColorize(arg)
+  })
+}
+
+function createNumConcat(
+  options: ColorizingOptions,
+): (text: TemplateStringsArray, n: number, plurals: Plurals) => Text {
+  return createSingleConcat(options, (string, arg, i, arr) => {
+    if (isPlurals(arg)) {
+      const n = arr[i - 1]
+      if (typeof n === 'number') {
+        return string + ngettext(n, arg)
+      }
+    }
+
+    return string + textUnitColorize(arg)
+  })
+}
+
+function isPlurals(arg: unknown): arg is Plurals {
+  return Array.isArray(arg) && typeof arg[0] === 'string' && typeof arg[1] === 'string' && typeof arg[2] === 'string'
 }
 
 interface ColorizingOptions {
@@ -83,4 +117,20 @@ export function textUnitColorize(unit: unknown, options: ColorizingOptions = {})
     case 'boolean':
       return unit ? '§fДа' : '§cНет'
   }
+}
+
+type Plurals = [one: string, two: string, five: string]
+
+/**
+ * Gets plural form based on provided number
+ *
+ * @param n - Number
+ * @param forms - Plurals forms in format `1 секунда 2 секунды 5 секунд`
+ * @returns Plural form. Currently only Russian supported
+ */
+function ngettext(n: number, [one = 'секунда', few = 'секунды', more = 'секунд']: Plurals) {
+  if (!Number.isInteger(n)) return more
+  return [one, few, more][
+    n % 10 == 1 && n % 100 != 11 ? 0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2
+  ]
 }
