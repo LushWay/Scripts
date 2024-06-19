44c3ca95fbfb8e0d46e3b07b1f74c92aa52d4736
leaftail1880 <110915645+leaftaul1880@users.noreply.github.com>
2024-06-19 16:49:54 +0300

    fix permissions

--- src/lib/rpg/menu.ts
+++ src/lib/rpg/menu.ts
@@ -151,6 +151,7 @@ export function createPublicGiveItemCommand(
   const command = new Command(name)
     .setDescription(`Выдает или убирает ${itemNameTag}§r§7§o из инвентаря`)
     .setGroup('public')
+    .setPermissions('member')
     .executes(ctx => {
       give(ctx.player)
     })
