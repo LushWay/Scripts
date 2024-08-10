8b4a71c93eef3de36a44013ea93a62dd6898c521
leaftail1880 <110915645+leaftaul1880@users.noreply.github.com>
2024-08-10 20:37:07 +0300

    chore: remove unused test

--- src/modules/features/cannon.ts
+++ src/modules/features/cannon.ts
@@ -1,15 +1,7 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { EasingType, system, world } from '@minecraft/server'
-import { registerAsync } from '@minecraft/server-gametest'
-import {
-  MinecraftItemTypes as i,
-  MinecraftBlockTypes,
-  MinecraftCameraPresetsTypes,
-  MinecraftEntityTypes,
-} from '@minecraft/vanilla-data'
+import { system, world } from '@minecraft/server'
+import { MinecraftItemTypes as i, MinecraftEntityTypes } from '@minecraft/vanilla-data'
 import { Vector } from 'lib'
 import { CustomItemWithBlueprint } from 'lib/rpg/custom-item'
-import { TestStructures } from 'test/constants'
 
 export const CannonItem = new CustomItemWithBlueprint('cannon')
   .typeId('lw:cannon_spawn_egg')
@@ -55,59 +47,4 @@ world.beforeEvents.playerInteractWithBlock.subscribe(event => {
   if (CannonBulletItem.isItem(event.itemStack)) {
     event.cancel = true
   }
-})
-
-registerAsync('ro', 'ro', async test => {
-  const xiller = world.getAllPlayers().find(e => !e.isSimulated())!
-  const dom = { x: -944.32, y: 64.0, z: 13840.3 }
-  await xiller.runCommandAsync('tp @a -944.75 65.00 13864.04 facing -944.32 64.00 13835.30')
-  await xiller.runCommandAsync('kill @e[type=lw:cannon]')
-
-  const camera = { x: -946.67, y: 65, z: 13865.94 }
-  xiller.camera.setCamera(MinecraftCameraPresetsTypes.Free, {
-    facingEntity: xiller,
-    location: camera,
-  })
-  await test.idle(1)
-  xiller.camera.setCamera(MinecraftCameraPresetsTypes.Free, {
-    facingLocation: dom,
-    location: Vector.add(camera, { x: 0, y: 0, z: -15 }),
-    easeOptions: {
-      easeTime: 8,
-      easeType: EasingType.OutCubic,
-    },
-  })
-
-  await test.idle(20 * 3)
-
-  await test.idle(20 * 4)
-
-  xiller.camera.setCamera(MinecraftCameraPresetsTypes.Free, {
-    facingLocation: xiller.location,
-    location: Vector.add(xiller.location, { x: 0, y: 2, z: -1 }),
-    easeOptions: {
-      easeTime: 0,
-      easeType: EasingType.Linear,
-    },
-  })
-
-  await test.idle(20 * 2)
-
-  xiller.camera.setCamera(MinecraftCameraPresetsTypes.Free, {
-    facingLocation: dom,
-    location: Vector.add(xiller.location, { x: 5, y: 2, z: 5 }),
-    easeOptions: {
-      easeTime: 1,
-      easeType: EasingType.Linear,
-    },
-  })
-
-  await test.idle(20 * 10)
-})
-  .structureName(TestStructures.empty)
-  .maxTicks(99999999)
-
-new Command('pla').executes(() => {
-  world.overworld.setBlockType({ x: -895.47, y: 68.0, z: 13861.5 }, MinecraftBlockTypes.Air)
-  world.overworld.setBlockType({ x: -895.47, y: 68.0, z: 13861.5 }, MinecraftBlockTypes.RedstoneBlock)
 })
