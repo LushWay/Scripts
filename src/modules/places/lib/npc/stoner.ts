019210d921abfd1bfde4a9af302653833c52f473
leaftail1880 <110915645+leaftaul1880@users.noreply.github.com>
2024-08-13 23:19:29 +0300

    feat: new stone types

--- src/modules/places/lib/npc/stoner.ts
+++ src/modules/places/lib/npc/stoner.ts
@@ -9,15 +9,41 @@ export class Stoner extends ShopNpc {
 
     this.shop.menu(form => {
       form
-        .addConfigurableItemSection(i.Andesite)
-        .defaultCount(1_000)
-        .maxCount(10_000)
-        .basePrice(1)
 
         .addConfigurableItemSection(i.Cobblestone)
         .defaultCount(1_000)
         .maxCount(20_000)
         .basePrice(2)
+
+        .addConfigurableItemSection(i.Andesite)
+        .defaultCount(1_000)
+        .maxCount(10_000)
+        .basePrice(1)
+
+        .addConfigurableItemSection(i.Diorite)
+        .defaultCount(2_000)
+        .maxCount(10_000)
+        .basePrice(1)
+
+        .addConfigurableItemSection(i.Granite)
+        .defaultCount(3_000)
+        .maxCount(10_000)
+        .basePrice(1)
+
+        .addConfigurableItemSection(i.Tuff)
+        .defaultCount(2_000)
+        .maxCount(10_000)
+        .basePrice(1)
+
+        .addConfigurableItemSection(i.Deepslate)
+        .defaultCount(100)
+        .maxCount(20_000)
+        .basePrice(2)
+
+        .addConfigurableItemSection(i.Calcite)
+        .defaultCount(10)
+        .maxCount(1000)
+        .basePrice(10)
     })
   }
 }
