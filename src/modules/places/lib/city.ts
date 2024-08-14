0a7aa09b8071d722b1ec972d6e4831494e8d254c
leaftail1880 <110915645+leaftaul1880@users.noreply.github.com>
2024-08-14 19:25:15 +0300

    feat: add scavenger

--- src/modules/places/lib/city.ts
+++ src/modules/places/lib/city.ts
@@ -4,6 +4,7 @@ import { Cutscene } from 'lib/cutscene'
 import { Quest } from 'lib/quest'
 import { t } from 'lib/text'
 import { Jeweler } from 'modules/places/lib/npc/jeweler'
+import { Scavenger } from 'modules/places/lib/npc/scavenger'
 import { PlaceWithSafeArea } from './place-with-safearea'
 
 export class City extends PlaceWithSafeArea {
@@ -23,4 +24,6 @@ export class City extends PlaceWithSafeArea {
   cutscene = new Cutscene(this.group.id, 'Исследование ' + this.name)
 
   jeweler = new Jeweler(this.group)
+
+  scavenger = new Scavenger(this.group)
 }
---
+++ src/modules/places/lib/npc/scavenger.ts
@@ -0,0 +1,16 @@
+import { ItemStack } from '@minecraft/server'
+import { MinecraftItemTypes as i } from '@minecraft/vanilla-data'
+import { Group } from 'lib/rpg/place'
+import { MoneyCost } from 'lib/shop/cost'
+import { ShopNpc } from 'lib/shop/npc'
+
+export class Scavenger extends ShopNpc {
+  constructor(group: Group) {
+    super(group.point('scavenger').name('Мусорщик'))
+    this.shop.body(() => 'Скоро ты сможешь прдавать мне весь свой мусор хеехех\n\n')
+
+    this.shop.menu(form => {
+      form.addItemStack(new ItemStack(i.OakSapling), new MoneyCost(1))
+    })
+  }
+}
