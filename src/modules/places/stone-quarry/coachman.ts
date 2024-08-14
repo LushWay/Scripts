02936ab14c3ad642ce9a351d4a4c724c900ae745
leaftail1880 <110915645+leaftaul1880@users.noreply.github.com>
2024-08-14 19:25:32 +0300

    feat: add coachman

---
+++ src/modules/places/stone-quarry/coachman.ts
@@ -0,0 +1,16 @@
+import { ItemStack } from '@minecraft/server'
+import { MinecraftItemTypes as i } from '@minecraft/vanilla-data'
+import { Group } from 'lib/rpg/place'
+import { MoneyCost } from 'lib/shop/cost'
+import { ShopNpc } from 'lib/shop/npc'
+
+export class Coachman extends ShopNpc {
+  constructor(group: Group) {
+    super(group.point('coachman').name('Кучер'))
+    this.shop.body(() => 'Wtf ой то есть мне пока нечего тебе предложить\n\n')
+
+    this.shop.menu(form => {
+      form.addItemStack(new ItemStack(i.HorseSpawnEgg), new MoneyCost(200_000))
+    })
+  }
+}
