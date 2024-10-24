72331e7bfd3afe0530f107d4f9dbeeb9cc66b9ca
leaftail1880 <110915645+leaftaul1880@users.noreply.github.com>
2024-10-24 07:58:03 +0300

    refactor: api and events separation

--- src/lib/bds/api.ts
+++ src/lib/bds/api.ts
@@ -1,4 +1,46 @@
-import type { ServerRpc } from './routes'
+import { t } from 'lib/text'
+import { inspect } from 'lib/util'
+import { ServerModules } from './modules'
+import { ServerRpc } from './routes'
+
+class RequestError extends Error {}
+
+/** Makes http request to node instance */
+export async function request<Path extends keyof ServerRpc.Routes>(
+  path: Path,
+  body: ServerRpc.Routes[Path]['req'],
+): Promise<ServerRpc.Routes[Path]['res']> {
+  console.info(t`request(${path}, ${body})`)
+
+  if (ServerModules.Net) {
+    const { http, HttpRequest, HttpRequestMethod } = ServerModules.Net
+
+    const stringifiedBody = body ? JSON.stringify(body) : ''
+    const response = await http.request(
+      new HttpRequest(`http://localhost:${__SERVER_PORT__}/` + path)
+        .setMethod(HttpRequestMethod.Post)
+        .addHeader('content-type', 'text/plain')
+        .addHeader('content-length', stringifiedBody.length.toString())
+        .setBody(stringifiedBody),
+    )
+
+    if (response.status === 404) {
+      throw new RequestError(`request(${path}): Unknown path!`)
+    }
+
+    if (response.status !== 200) {
+      throw new RequestError(`request(${path}): Unknown status ${response.status}. Expected 200.`)
+    }
+
+    try {
+      return (response.body ? JSON.parse(response.body) : undefined) as ServerRpc.Routes[Path]['res']
+    } catch (error) {
+      throw new RequestError(
+        t.error`request(${path}): Failed to parse NodeServer response.body(${inspect(response.body)}): ${error}`,
+      )
+    }
+  } else console.error('NET MODULE IS DISABLED, SKIPPING')
+}
 
 /**
  * Sends packet message to the core nodejs process running BDS. Used mostly for console/chat logging purposes, when data
