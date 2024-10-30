f5df2ef7184033d5df5a11a5c9b62f0f1b46b8ea
leaftail1880 <110915645+leaftaul1880@users.noreply.github.com>
2024-10-30 20:42:34 +0300

    fix: blending

--- src/modules/world-edit/utils/blending.ts
+++ src/modules/world-edit/utils/blending.ts
@@ -2,34 +2,33 @@ import { LimitedSet } from 'lib/database/persistent-set'
 import { Vector } from 'lib/vector'
 
 const doNotBlendStorage = new LimitedSet<string>(1_000)
-const dontBlend = false
 
 export function skipForBlending(
   { blending, radius, factor }: { blending: number; radius: number; factor: number },
   { vector, center }: { vector: Vector3; center: Vector3 },
 ) {
-  if (blending === -1) return dontBlend
+  const vectorId = Vector.string(vector)
+  const dontBlend = () => {
+    doNotBlendStorage.add(vectorId)
+    return false
+  }
+
+  if (blending === -1) return dontBlend()
 
   const distance = ~~Vector.distance(vector, center)
 
-  if (blending === 0) {
-    // Outside of circle, skip
-    if (distance > radius) return true
-  } else {
+  // Outside of circle, skip
+  if (distance > radius) return true
+  if (blending === 0) return dontBlend()
+  else {
     // Blending
     const toBlend = radius - blending
-    const vectorId = Vector.string(vector)
 
     if (radius > toBlend) {
       if (doNotBlendStorage.has(vectorId)) return true
 
       const blendingFactor = 1 + factor * 0.01
-      if (Math.randomInt(toBlend, radius) < distance * blendingFactor) {
-        return true
-      }
-    } else {
-      doNotBlendStorage.add(vectorId)
-      return true
-    }
+      if (Math.randomInt(toBlend, radius) < distance * blendingFactor) return true
+    } else return dontBlend()
   }
 }
