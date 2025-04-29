610eb852761efaed05883dcb70071bb85677c417
leaftail1880 <110915645+leaftaul1880@users.noreply.github.com>
2025-04-29 10:05:40 +0300

    update message2.ts

--- src/lib/load/message2.ts
+++ src/lib/load/message2.ts
@@ -5,5 +5,10 @@ const message = `§9> §fDone in §6${((Date.now() - globalThis.loaded) / 1000).
 system.delay(() => {
   console.info(message)
   if (!__RELEASE__) world.say(message)
+
+  import('lib/roles').then(({ is }) => {
+    for (const player of world.getAllPlayers()) if (is(player.id, 'techAdmin')) player.tell(`§sCommit: §f${__GIT__}`)
+  })
+
   globalThis.loaded = 0
 })
