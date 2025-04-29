1049f87544f54f4958cd1ab17f2255410552408b
leaftail1880 <110915645+leaftaul1880@users.noreply.github.com>
2025-04-30 00:52:08 +0300

    test: fix tests

--- src/test/utils.ts
+++ src/test/utils.ts
@@ -18,8 +18,8 @@ interface RejectableFunction<T extends keyof TFD> extends TestFormCallback<T> {
 
 export function TEST_onFormOpen<T extends keyof TFD>(player: Player, kind: T, callback: TestFormCallback<T>) {
   const onForm = ((player as TestPlayer).onForm ??= {})
-  const previousCallback = onForm[kind] as RejectableFunction<T>
-  previousCallback.reject?.(new Error('Form listen request was canceled by next call'))
+  const previousCallback = onForm[kind] as RejectableFunction<T> | undefined
+  previousCallback?.reject?.(new Error('Form listen request was canceled by next call'))
 
   const { resolve, reject, promise } = Promise.withResolvers<void>()
   const wrappedCallback: RejectableFunction<T> = (...args) => {
