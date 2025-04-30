cb9e430ef2b33b3820400f950737785c116278f4
leaftail1880 <110915645+leaftaul1880@users.noreply.github.com>
2025-04-30 13:22:36 +0300

    feat: create break-item-durability-notify.ts update import.ts

---
+++ src/modules/features/break-item-durability-notify.ts
@@ -0,0 +1,42 @@
+import { system } from '@minecraft/server'
+import { actionGuard, ActionGuardOrder, Menu, Settings } from 'lib'
+import { ActionbarPriority } from 'lib/extensions/on-screen-display'
+import { t } from 'lib/text'
+
+const playerSettings = Settings.player(...Menu.settings, {
+  breakItemDurabilityNotify: {
+    name: 'Уведомлять о ломающемся предмете',
+    description:
+      'Если включено, то когда вы взаимодействуете с блоком предметом, у которого остается менее 90%% прочности, показывать уведомление на весь экран',
+    value: true,
+  },
+  // TODO Donut feature
+  breakItemDurabilityCancel: {
+    name: 'Не давать ломать предмет',
+    description:
+      'Если включено, то когда вы взаимодействуете с блоком предметом, у которого остается менее 99%% прочности, взаимодействие будет отменено, а предмет - спасен.',
+    value: true,
+  },
+})
+
+actionGuard((player, _, ctx) => {
+  const settings = playerSettings(player)
+  if (!settings.breakItemDurabilityNotify) return
+  if (ctx.type !== 'break') return
+
+  const durability = ctx.event.itemStack?.durability
+  if (!durability) return
+
+  const { damage, maxDurability } = durability
+  const percent = (damage / maxDurability) * 100
+
+  if (settings.breakItemDurabilityCancel && percent >= 99) return false
+
+  system.delay(() => {
+    if (percent >= 90)
+      player.onScreenDisplay.setActionBar(
+        t.error`§cИнструмент скоро сломается! ${damage}/${maxDurability} (${percent}%)`,
+        ActionbarPriority.UrgentNotificiation,
+      )
+  })
+}, ActionGuardOrder.Feature)
--- src/modules/features/import.ts
+++ src/modules/features/import.ts
@@ -2,5 +2,6 @@ import '../places/anarchy/quartz'
 import '../places/base/base'
 import '../places/base/base-menu'
 
+import './break-item-durability-notify'
 import './break-place-outside-of-region'
 import './ender-chest'
