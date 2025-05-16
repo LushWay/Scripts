cb060ede9b3f57f2682f73dd5c965d3bd0002c4c
leaftail1880 <110915645+leaftail1880@users.noreply.github.com>
2025-05-16 21:28:35 +0300

    Make wither region go boom

--- src/modules/pvp/raid.ts
+++ src/modules/pvp/raid.ts
@@ -5,6 +5,7 @@ import { MineareaRegion } from 'lib/region/kinds/minearea'
 import { isScheduledToPlace } from 'lib/scheduled-block-place'
 import { t } from 'lib/text'
 import { BaseRegion } from 'modules/places/base/region'
+import { StoneQuarry } from 'modules/places/stone-quarry/stone-quarry'
 
 const notify = new Map<string, { time: number; reason: string }>()
 const targetLockTime = ms.from('min', 8)
@@ -15,7 +16,7 @@ world.beforeEvents.explosion.subscribe(event => {
   let base = false
   const impactedBlocks = event.getImpactedBlocks().filter(block => {
     const region = Region.getAt(block)
-    if (region instanceof MineareaRegion) return true
+    if (region instanceof MineareaRegion || region === StoneQuarry.wither.region) return true
     if (isScheduledToPlace(block, block.dimension.type)) return true
     if (region instanceof BaseRegion) {
       for (const id of region.permissions.owners) notify.set(id, { time: targetLockTime, reason: 'вас рейдят' })
