796448e4eea0e9c9055e876c8ce195e0058e0e0b
leaftail1880 <110915645+leaftail1880@users.noreply.github.com>
2025-05-21 23:29:37 +0300

    abc

--- src/modules/features/cannon.ts
+++ src/modules/features/cannon.ts
@@ -18,6 +18,19 @@ const cooldown = new Cooldown(ms.from('sec', 5))
 actionGuard((player, _, ctx) => {
   if (ctx.type !== 'interactWithEntity' && ctx.type !== 'interactWithBlock') return
 
+  return fire(player)
+}, ActionGuardOrder.Feature)
+
+function guide(player: Player) {
+  system.delay(() => player.fail(t.error`Используй этот предмет сидя на пушке`))
+  return false
+}
+
+world.beforeEvents.itemUse.subscribe(event => {
+  event.cancel = !fire(event.source, true)
+})
+
+function fire(player: Player, fire = false) {
   if (!CannonShellItem.isItem(player.mainhand().getItem())) return
 
   const riding = player.getComponent(EntityComponentTypes.Riding)
@@ -25,27 +38,26 @@ actionGuard((player, _, ctx) => {
   if (!cannon || cannon.typeId !== CustomEntityTypes.Cannon) return guide(player)
   if (!cooldown.isExpired(player)) return false
 
-  system.delay(() => {
-    const mainhand = player.mainhand()
-    if (mainhand.isValid) {
-      if (mainhand.amount === 1) mainhand.setItem(undefined)
-      else mainhand.amount--
-    }
+  if (fire)
+    system.delay(() => {
+      const mainhand = player.mainhand()
+      if (mainhand.isValid) {
+        if (mainhand.amount === 1) mainhand.setItem(undefined)
+        else mainhand.amount--
+      }
 
-    if (cannon.isValid) {
-      const view = cannon.getViewDirection()
-      const location = Vector.add(Vector.add(cannon.location, Vector.multiply(view, 2.5)), { x: 0, y: 1.5, z: 0 })
-      const tnt = cannon.dimension.spawnEntity(MinecraftEntityTypes.Tnt, Vector.add(location, { x: 0, y: -0.5, z: 0 }))
-      tnt.applyImpulse(Vector.multiply(player.getViewDirection(), 2))
-      cannon.dimension.playSound('random.explode', location, { volume: 4, pitch: 0.9 })
-      cannon.dimension.spawnParticle('minecraft:dragon_dying_explosion', location)
-    }
-  })
+      if (cannon.isValid) {
+        const view = cannon.getViewDirection()
+        const location = Vector.add(Vector.add(cannon.location, Vector.multiply(view, 2.5)), { x: 0, y: 1.5, z: 0 })
+        const tnt = cannon.dimension.spawnEntity(
+          MinecraftEntityTypes.Tnt,
+          Vector.add(location, { x: 0, y: -0.5, z: 0 }),
+        )
+        tnt.applyImpulse(Vector.multiply(player.getViewDirection(), 2))
+        cannon.dimension.playSound('random.explode', location, { volume: 4, pitch: 0.9 })
+        cannon.dimension.spawnParticle('minecraft:dragon_dying_explosion', location)
+      }
+    })
 
-  return false
-}, ActionGuardOrder.Feature)
-
-function guide(player: Player) {
-  system.delay(() => player.fail(t.error`Используй этот предмет сидя на пушке`))
   return false
 }
