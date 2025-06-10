a32c014106eb0b853774f6c56d4a07bb6abdbe09
leaftail1880 <110915645+leaftaul1880@users.noreply.github.com>
2025-06-10 13:35:40 +0300

    test: fix tests tz

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -6,3 +6,12 @@ import 'lib/command'
 
 import { Area } from 'lib/region/areas/area'
 Area.loaded = false
+
+// Determenstic test results across all enviroments
+if ('process' in globalThis) {
+  // @ts-expect-error No node types installed because they pollute globals
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  process.env.TZ = 'Europe/Moscow'
+}
+
+
