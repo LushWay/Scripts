f42963c461ff1177b16a16168c6f252034692ca1
leaftail1880 <110915645+leaftail1880@users.noreply.github.com>
2025-06-18 18:33:48 +0300

    update blocks-set.ts

--- src/modules/world-edit/utils/blocks-set.ts
+++ src/modules/world-edit/utils/blocks-set.ts
@@ -33,9 +33,9 @@ const blocksSets = table<BlocksSets>('blockSets', () => ({}))
 // GENERAL MANIPULATIONS
 
 export function getOtherPlayerBlocksSets(playerId: string): [string, BlocksSets][] {
-  return Object.entries(blocksSets).filter(
-    (e => e[0] !== playerId && e[1]) as (e: [string, BlocksSets | undefined]) => e is [string, BlocksSets],
-  )
+  return blocksSets
+    .entries()
+    .filter((e => e[0] !== playerId && e[1]) as (e: [string, BlocksSets | undefined]) => e is [string, BlocksSets])
 }
 
 export function getAllBlocksSets(id: string): BlocksSets {
