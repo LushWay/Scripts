8169a3b3187d78b2e765e38b0d0b1e8aaa3ac130
leaftail1880 <110915645+leaftail1880@users.noreply.github.com>
2025-06-18 19:57:51 +0300

    update structure.ts

--- src/lib/region/structure.ts
+++ src/lib/region/structure.ts
@@ -98,7 +98,7 @@ export class RegionStructure {
     const structure = world.structureManager.get(this.id)
     if (!structure) throw new ReferenceError('No structure found!')
 
-    const [, from] = this.region.area.edges
+    const [from] = this.region.area.edges
     const offset = this.offset ? { x: this.offset, y: this.offset, z: this.offset } : undefined
 
     return this.region.area.forEachVector((vector, isIn, dimension) => {
