386e17409e304ac87fa2f9570cdca0104e9206b7
leaftail1880 <110915645+leaftail1880@users.noreply.github.com>
2025-09-08 14:28:20 +0300

    fix small bugs

--- src/lib/utils/logger.ts
+++ src/lib/utils/logger.ts
@@ -3,40 +3,44 @@ import { WeakPlayerMap } from 'lib/weak-player-storage'
 import { noI18n } from '../i18n/text'
 
 /** Creates new logger that will use name as prefix and will output info to the console */
-export function createLogger(name: string) {
-  name = name.startsWith('§9') ? name : `§9${name}`
+function createSimpleLogger(name: string) {
   return {
     debug: createLevel(name, 'debug', debug),
     info: createLevel(name, 'info', info),
     error: createLevel(name, 'error', error),
     warn: createLevel(name, 'warn', warn),
-    player: (player: Player) => {
-      const newName = `${name.trimEnd()} §f§l${player.name}§r `
-      const cache = playerLoggers.get(newName)
-      if (cache) {
-        return cache
-      } else {
-        const logger = createLogger(newName)
-        playerLoggers.set(newName, logger)
-        return logger
-      }
-    },
   }
 }
 
-type Logger = ReturnType<typeof createLogger>
+type SimpleLogger = ReturnType<typeof createSimpleLogger>
 
-const playerLoggers = new WeakPlayerMap<Logger>()
+/** Creates new logger that will use name as prefix and will output info to the console */
+export function createLogger(name: string) {
+  const playerLoggers = new WeakPlayerMap<SimpleLogger>()
+  name = name.startsWith('§9') ? name : `§9${name}`
+
+  return {
+    ...createSimpleLogger(name),
+    player: (player: Player) => {
+      const cache = playerLoggers.get(player.id)
+      if (cache) return cache
+
+      const logger = createSimpleLogger(`${name.trimEnd()} §f§l${player.isValid ? player.name : player.id}§r `)
+      playerLoggers.set(player.id, logger)
+      return logger
+    },
+  }
+}
 
 const debug = noI18n.restyle({ unit: '§l§6', text: '§f' })
 const info = noI18n
 const warn = noI18n.warn
 const error = noI18n.error
 
-function createLevel(name: string, level: 'debug' | 'error' | 'info' | 'warn', text: Text.Fn<string, unknown>) {
+function createLevel(name: string, level: 'debug' | 'error' | 'info' | 'warn', style: Text.Fn<string, unknown>) {
   return (t: unknown, ...args: unknown[]) => {
     if (isTemplateStringsArray(t)) {
-      console[level](name, text(t, ...args))
+      console[level](name, style(t, ...args))
     } else {
       console[level](name, t, ...args)
     }
