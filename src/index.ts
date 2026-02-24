17360ba09bb58aff5ee3596a0d54293e2a452b3c
leaftail1880 <110915645+leaftail1880@users.noreply.github.com>
2026-02-24 15:24:45 +0300

    update src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ import 'lib/assets/intl-global-object'
 import 'lib/assets/intl'
 
 import('./modules/loader').catch((e: unknown) => {
+  console.log('hello from autodeply')
   console.error('Loading error', e)
 })
