52ad184d02f449357a5ae607806a622ee7daaac0
leaftail1880 <110915645+leaftail1880@users.noreply.github.com>
2026-02-25 11:02:15 +0300

    delete 1 file and update 1 file

--- tools/patches.ts
+++ tools/patches.ts
@@ -1,9 +1,4 @@
-import { m, notice, patchPackage } from './patch-package.ts'
-
-export type ________ = typeof import('@minecraft/server')
-
 patchPackage('@minecraft/server', {
-  classes: {},
   replaces: [
     {
       // use name from the function name instead, make eslint rule for it
@@ -15,20 +10,73 @@ patchPackage('@minecraft/server', {
       replace: 'runTimeout(callback: () => void, name: string, tickDelay?: number): number;',
     },
     {
-      find: m` * @throws
+      find: ` 
+     * @throws
      * Throws if the slot's container is invalid.
      *
      * {@link InvalidContainerSlotError}
      */
-    readonly typeId: string;`,
+    readonly typeId: string;`.trim(),
       replace: `
      */
     readonly typeId?: string;`,
     },
   ],
-  additions: {
-    beginning: '',
-    afterImports: notice,
-    ending: notice,
-  },
 })
+
+import fs from 'fs/promises'
+import path from 'path'
+
+import { pathInfo } from 'leafy-utils'
+import { createRequire } from 'module'
+
+const noticeFirstLine = '* This file was automatically patched by'
+export const notice = `/**
+${noticeFirstLine}
+* tools/patch-package.ts
+*
+* New method assigments can be found in the
+* src/lib/extensions/
+*/`
+
+export const { relative } = pathInfo(import.meta.url)
+const require = createRequire(import.meta.url)
+export const resolve = require.resolve
+
+export async function patchPackage(
+  packageName: string,
+  options: {
+    replaces: { find: RegExp | string; replace: string; all?: boolean; throwError?: boolean }[]
+  },
+) {
+  const packageJsonPath = path.join(packageName, 'package.json')
+  let packagePath
+  try {
+    packagePath = resolve(packageJsonPath)
+  } catch {
+    console.log('Unable to resolve', packageJsonPath)
+    return
+  }
+
+  // Get path to the package's TypeScript definition file
+  const indexDts = path.join(packagePath, '..', 'index.d.ts')
+
+  let patchedCode = (await fs.readFile(indexDts, 'utf-8')).replaceAll('\r\n', '\n')
+  if (patchedCode.includes(noticeFirstLine)) return console.log('Already patched')
+
+  // Apply the replacements
+  for (const { all, find, replace, throwError } of options.replaces) {
+    const newCode = patchedCode[all ? 'replaceAll' : 'replace'](find, replace)
+
+    if (newCode !== patchedCode) {
+      patchedCode = newCode
+    } else {
+      if (throwError !== false) throw new Error(`Unable to find replace for ${find}`)
+    }
+  }
+
+  let newCode = `${notice}\n${patchedCode}\n${notice}`
+
+  // Write the patched code back to the file
+  await fs.writeFile(indexDts, newCode)
+}
